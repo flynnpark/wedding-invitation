8ea444a09506786178b025a9d2929b7358a4a555
Flynn <wphestiraid@gmail.com>
2022-08-07 19:34:17 +0900

    feat: 메인 섹션 타이틀 수정

--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -8,10 +8,10 @@ function Home() {
     <Section>
       <div className="block items-center justify-center  max-w-xl mx-auto w-full relative my-20">
         <div className="absolute left-1/2 top-28 text-center -translate-x-1/2">
-          <h1 className="text-xl text-white mb-4">
+          <h1 className="text-base text-white underline underline-offset-2 decoration-1 shadow-black drop-shadow-[0_3px_3px_rgba(0,0,0,8)]">
             Save the date for the wedding
           </h1>
-          <span className="text-gray-900 font-medium bg-white bg-opacity-70 py-2 px-4 text-3xl ">
+          <span className="text-gray-900 font-medium bg-white bg-opacity-70 py-2 px-4 text-3xl inline-block mt-2">
             박인호 <span className="text-xl">&</span> 박아름
           </span>
         </div>
