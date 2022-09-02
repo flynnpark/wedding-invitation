5fc0d5b13fabcf64fde1beaf997161a9cb52fe22
Flynn <wphestiraid@gmail.com>
2022-09-02 10:01:37 +0900

    feat: 방명록 컴포넌트 구조 수정

---
+++ src/components/guestbook/WriteForm.tsx
@@ -0,0 +1,135 @@
+import { addDoc, collection } from 'firebase/firestore';
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import Modal from 'react-modal';
+import { toast } from 'react-toastify';
+
+import { db } from 'utils/firebase';
+
+export interface GuestBookForm {
+  name: string;
+  password: string;
+  content: string;
+}
+
+interface WriteFormProps {
+  isOpen: boolean;
+  handleClose: () => void;
+
+  setIsOpen: (value: React.SetStateAction<boolean>) => void;
+  addNewPost: (id: string, name: string, content: string) => void;
+}
+
+function WriteForm({
+  isOpen,
+  handleClose,
+  setIsOpen,
+  addNewPost,
+}: WriteFormProps) {
+  const { register, handleSubmit, reset } = useForm<GuestBookForm>();
+
+  const onValid = async (data: GuestBookForm) => {
+    window.gtag?.('event', 'write_guest_book');
+
+    const { name, password, content } = data;
+    const docRef = await addDoc(collection(db, 'guestBook'), {
+      name,
+      password,
+      content,
+      isDeleted: false,
+      createdAt: new Date(),
+    });
+    addNewPost(docRef.id, name, content);
+    setIsOpen(false);
+    toast.info('게시글이 작성되었어요!', {
+      position: 'bottom-center',
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+    reset();
+  };
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      className="bg-white rounded-2xl flex flex-col p-4 w-80"
+      overlayClassName="fixed top-0 left-0 right-0 bottom-0 bg-black/[.40] items-center justify-center flex h-full"
+    >
+      <div className="text-center">
+        <h2>방명록 글 작성</h2>
+        <hr className="my-4" />
+      </div>
+      <form className="flex flex-col text-sm" onSubmit={handleSubmit(onValid)}>
+        <div className="space-y-4">
+          <div className="w-full flex flex-col">
+            <label
+              htmlFor="name"
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2 font-sans"
+            >
+              이름
+            </label>
+            <input
+              type="text"
+              id="name"
+              {...register('name', { required: true })}
+              className="bg-stone-200 rounded-xl px-2 py-1"
+            />
+          </div>
+          <div className="w-full flex flex-col">
+            <div>
+              <label
+                htmlFor="name"
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2 font-sans"
+              >
+                비밀번호
+                <span className="inline-block ml-2 font-medium text-gray-400">
+                  글 수정, 삭제만을 위해 사용되는 비밀번호
+                </span>
+              </label>
+            </div>
+            <input
+              type="password"
+              id="password"
+              {...register('password', { required: true })}
+              className="bg-stone-200 rounded-xl px-2 py-1"
+            />
+          </div>
+          <div className="flex flex-col space-y-2">
+            <label
+              htmlFor="name"
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold font-sans"
+            >
+              내용
+            </label>
+            <textarea
+              id="content"
+              rows={5}
+              {...register('content', { required: true })}
+              className="bg-stone-200 rounded-xl px-2 py-1"
+            />
+          </div>
+        </div>
+        <div className="flex flex-row space-x-2 justify-center mt-4">
+          <button
+            type="submit"
+            className="flex bg-stone-600  py-2 rounded-md justify-center text-sm w-24 text-white"
+          >
+            작성하기
+          </button>
+          <button
+            className="flex bg-stone-300  py-2 rounded-md justify-center text-sm w-24"
+            onClick={handleClose}
+          >
+            닫기
+          </button>
+        </div>
+      </form>
+    </Modal>
+  );
+}
+
+export default WriteForm;
--- src/sections/GuestBook.tsx
+++ src/sections/GuestBook.tsx
@@ -1,32 +1,25 @@
-import {
-  addDoc,
-  collection,
-  getDocs,
-  limit,
-  orderBy,
-  query,
-} from 'firebase/firestore';
+import { collection, getDocs, limit, orderBy, query } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react';
-import { useForm } from 'react-hook-form';
 
+import WriteForm from 'components/guestbook/WriteForm';
 import Section from 'components/Section';
 import { db } from 'utils/firebase';
 
-interface GuestBookForm {
-  name: string;
-  password: string;
-  content: string;
-}
-
-interface Post {
+export interface Post {
   id: string;
   name: string;
   content: string;
 }
 
 function GuestBook() {
-  const { register, handleSubmit, reset } = useForm<GuestBookForm>();
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const [posts, setPosts] = useState<Post[]>([]);
+
+  const handleModalOpen = () => {
+    window.gtag?.('event', 'open_guestbook_form');
+    setIsModalOpen(true);
+  };
+  const handleModalClose = () => setIsModalOpen(false);
 
   const fetchData = async () => {
     const dataQuery = query(
@@ -47,68 +40,50 @@ function GuestBook() {
     fetchData();
   }, []);
 
-  const onValid = async (data: GuestBookForm) => {
-    console.log(data);
-    const { name, password, content } = data;
-    const docRef = await addDoc(collection(db, 'guestBook'), {
-      name,
-      password,
-      content,
-      isDeleted: false,
-      createdAt: new Date(),
-    });
+  const addNewPost = (id: string, name: string, content: string) => {
     setPosts([
       {
-        id: docRef.id,
+        id,
         name,
         content,
       },
       ...posts,
     ]);
-    reset();
   };
 
   return (
     <Section>
       <div className="w-full max-w-2xl mx-auto space-y-12 px-4">
         <h1 className="text-3xl text-center">방명록</h1>
-        <form className="mt-20" onSubmit={handleSubmit(onValid)}>
-          <label htmlFor="name">이름</label>
-          <input
-            type="text"
-            id="name"
-            {...register('name', { required: true })}
-          />
-          <label htmlFor="name">비밀번호</label>
-          <input
-            type="password"
-            id="password"
-            {...register('password', { required: true })}
-          />
-          <div>
-            <label htmlFor="name">내용</label>
-            <input
-              type="text"
-              id="content"
-              {...register('content', { required: true })}
-            />
-          </div>
-          <div>
-            <button type="submit">작성하기</button>
-          </div>
-        </form>
-        <div className="overflow-x-auto flex space-x-2 h-64 py-4 pl-1">
-          {posts.map(({ id, name, content }) => (
-            <div
-              key={id}
-              className="flex flex-col rounded-xl w-44 max-w-xl bg-stone-100 flex-none text-xs p-4 justify-between shadow-md"
-            >
-              <div className="overflow-hidden flex-col break-all">
-                {content}
+        <WriteForm
+          isOpen={isModalOpen}
+          handleClose={handleModalClose}
+          setIsOpen={setIsModalOpen}
+          addNewPost={addNewPost}
+        />
+        <div className="space-y-4">
+          <div className="overflow-x-auto flex space-x-3 h-64 py-4 pl-1 relative -left-1">
+            {posts.map(({ id, name, content }) => (
+              <div
+                key={id}
+                className="flex flex-col rounded-xl w-44 max-w-xl bg-stone-100 flex-none text-xs p-4 justify-between shadow-md"
+              >
+                <div className="overflow-hidden flex-col break-all">
+                  {content}
+                </div>
+                <h1 className="text-center">- {name} -</h1>
               </div>
-              <h1 className="text-center">- {name} -</h1>
-            </div>
-          ))}
+            ))}
+          </div>
+          <div className="flex flex-row justify-end space-x-2 text-sm font-sans">
+            <button className="p-1">전체 보기</button>
+            <button
+              className="p-1 font-bold flex-row"
+              onClick={handleModalOpen}
+            >
+              글 남기기
+            </button>
+          </div>
         </div>
       </div>
     </Section>
